1e68e0cb71bc0401d93ae8664a64751f59934a17
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2023-05-01 17:29:45 +0800

    Update main.py

--- algorithms/serialization_task/main.py
+++ algorithms/serialization_task/main.py
@@ -1,1 +1,72 @@
+from enum import Enum
+import pickle
 
+class Alignment(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+class Widget():
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.childrens = []
+        if self.parent is not None:
+            self.parent.add_children(self)
+
+    def add_children(self, children: "Widget"):
+        self.childrens.append(children)
+
+    def to_binary(self):
+        return pickle.dumps(self)
+
+    @classmethod
+    def from_binary(cls, data):
+        return pickle.loads(data)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}{self.childrens}"
+
+    def __repr__(self):
+        return str(self)
+
+class MainWindow(Widget):
+
+    def __init__(self, title: str):
+        super().__init__(None)
+        self.title = title
+
+class Layout(Widget):
+
+    def __init__(self, parent, alignment: Alignment):
+        super().__init__(parent)
+        self.alignment = alignment
+
+class LineEdit(Widget):
+
+    def __init__(self, parent, max_length: int=10):
+        super().__init__(parent)
+        self.max_length = max_length
+
+class ComboBox(Widget):
+
+    def __init__(self, parent, items):
+        super().__init__(parent)
+        self.items = items
+
+app = MainWindow("Application")
+layout1 = Layout(app, Alignment.HORIZONTAL)
+layout2 = Layout(app, Alignment.VERTICAL)
+
+edit1 = LineEdit(layout1, 20)
+edit2 = LineEdit(layout1, 30)
+
+box1 = ComboBox(layout2, [1, 2, 3, 4])
+box2 = ComboBox(layout2, ["a", "b", "c"])
+
+print(app)
+
+bts = app.to_binary()
+print(f"Binary data length {len(bts)}")
+
+new_app = MainWindow.from_binary(bts)
+print(new_app)
